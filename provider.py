9c4474b35bc6117bbfced68b170df74b0265dce8
Matthias Binzer <matgnt@gmail.com>
2022-10-03 20:16:29 +0200

    pyopenssl accept regular https

--- provider.py
+++ provider.py
@@ -3,11 +3,12 @@
 import asyncio
 from genericpath import isfile
 import os, sys
-import websockets
+import wsproto
 from datetime import datetime
 import json
 import pathlib
-import ssl
+import socket
+from OpenSSL import SSL
 import logging
 
 from pyeebus import x509_utils
@@ -21,7 +22,7 @@ PROVIDER_PRIVATE_KEY_FN = os.getenv('PROVIDER_PRIVATE_KEY_FN', 'provider.key')
 PROVIDER_PUBLIC_KEY_FN = os.getenv('PROVIDER_PUBLIC_KEY_FN', 'provider.pem')
 PROVIDER_CERT_FN = os.getenv('PROVIDER_CERT_FN', 'provider.crt')
 
-WEBSOCKET_HOST = os.getenv('WEBSOCKET_HOST', 'localhost')
+WEBSOCKET_HOST = os.getenv('WEBSOCKET_HOST', '127.0.0.1')
 WEBSOCKET_PORT = int(os.getenv('WEBSOCKET_PORT', '8765'))
 
 
@@ -38,58 +39,30 @@ assert os.path.isfile(PROVIDER_CERT_FN)
 
 connections = {}
 
-#ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
-#ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-#ssl_context.verify_mode = ssl.CERT_REQUIRED
-#ssl_context.check_hostname = False
-#ssl_context.load_cert_chain(certfile=PROVIDER_CERT_FN, keyfile=PROVIDER_PRIVATE_KEY_FN)
-#ssl_context.load_verify_locations(PROVIDER_CERT_FN) # TODO: do we need this?
+def verify_cb(conn, cert, err, depth, ok):
+    print(cert)
 
-#ssl_context = ssl._create_unverified_context(purpose=ssl.Purpose.CLIENT_AUTH, certfile=PROVIDER_CERT_FN, keyfile=PROVIDER_PRIVATE_KEY_FN, cafile=PROVIDER_CERT_FN)
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-ssl_context.load_cert_chain
-ssl_context.post_handshake_auth = True
+    return 1 # TODO: security: 1 means cert check ok!
 
+ssl_context = SSL.Context(SSL.SSLv23_METHOD)
+ssl_context.set_cipher_list(b"TLS_AES_128_GCM_SHA256:AES128-GCM-SHA256") # do we need this?
+ssl_context.set_verify(SSL.VERIFY_PEER, callback=verify_cb)
+ssl_context.use_certificate_file(certfile=PROVIDER_CERT_FN)
+ssl_context.use_privatekey_file(keyfile=PROVIDER_PRIVATE_KEY_FN)
 
-async def send_data():
-    while True:
-        for id, ws in connections.items():
-            data = {
-                'id': id,
-                'timestamp': datetime.now().isoformat(), 
-            }
-            await ws.send(json.dumps(data))
-        await asyncio.sleep(5.0)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-async def handler(websocket):
-    # new connection
-    global connections
-    connections[str(websocket.id)] = websocket
-    websocket.send('added to list')
-    peercert = websocket.transport.get_extra_info('peercert')
-    print(peercert)
-    async for msg in websocket:
-        print(msg)
+conn = SSL.Connection(ssl_context, sock)
+sock.bind(('', WEBSOCKET_PORT))
+sock.listen(5)
 
-async def connection_request(path, request_headers):
-    """
-    Ref: https://websockets.readthedocs.io/en/stable/reference/server.html#websockets.server.WebSocketServerProtocol.process_request
-    """
-    # try checking mTLS params here
-
-    return None # None to continue with regular handshake
-
-async def start_websocket():
-    async with websockets.serve(handler, WEBSOCKET_HOST, WEBSOCKET_PORT, ssl=ssl_context, process_request=connection_request):
-    #async with websockets.serve(handler, WEBSOCKET_HOST, WEBSOCKET_PORT, process_request=connection_request):
-        await asyncio.Future()
-
-async def main():
-    await asyncio.gather(
-        start_websocket(),
-        send_data()
-    )
-    print('xxx')
-
-if __name__ == "__main__":
-    asyncio.run(main())
+while True:
+    incoming_sock, fromaddr = sock.accept()
+    incoming_ssl_conn = SSL.Connection(ssl_context, incoming_sock)
+    incoming_ssl_conn.set_accept_state()
+    incoming_ssl_conn.do_handshake()
+    print(f"List of ciphers: {incoming_ssl_conn.get_cipher_list()}")
+    req = incoming_ssl_conn.read(4096)
+    print(req)
+    incoming_ssl_conn.write(b"HTTP/1.1 200 OK\r\nServer: my-special\r\nContent-length: 10\r\n\r\nHello!\r\n\r\n")
+    incoming_ssl_conn.set_shutdown(SSL.SENT_SHUTDOWN)
